a3e96d4275418beb4705eeb778ff21874f01098f
taaaf11 <taafuuu@gmail.com>
2024-12-28 14:37:12 +0500

    Modify section holder control to include catppuccin colors

--- ui/sections_holder_control.py
+++ ui/sections_holder_control.py
@@ -1,3 +1,6 @@
+from collections.abc import Callable
+
+import catppuccin
 import flet as ft
 
 from model import AnimatableSectionProtocol
@@ -12,6 +15,8 @@ class SectionsHolderControl(ft.Container):
             ft.Container(
                 ft.Text(section.section_header),
                 ink=True,
+                padding=13,
+                border_radius=12,
                 data=section.section_header,
                 on_click=self._on_section_button_click
             )
@@ -37,10 +42,30 @@ class SectionsHolderControl(ft.Container):
         self.alignment = ft.alignment.center
         # self.border = ft.border.all(1)
 
+    def did_mount(self):
+        first_tab = self.tabs[0]
+
+        first_tab.bgcolor = catppuccin.PALETTE.frappe.colors.pink.hex
+        first_tab.content.color = catppuccin.PALETTE.frappe.colors.crust.hex
+        first_tab.update()
+
     def _on_section_button_click(self, e):
         section: AnimatableSectionProtocol
+        e.control: ft.Container
+
         for section in self.sections:
             is_true_section = section.section_header == e.control.data
+
+            for tab in self.tabs:
+                tab.bgcolor = ft.Colors.TRANSPARENT
+                tab.content.color = None
+
             if is_true_section:
+                # updating tab color
+                e.control.bgcolor = "#E6" + catppuccin.PALETTE.frappe.colors.pink.hex[1:]
+                e.control.content.color = catppuccin.PALETTE.frappe.colors.crust.hex
+                e.control.update()
+
+                # updating section content
                 self.main_content.content = section
                 self.update()
