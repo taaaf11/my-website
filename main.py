f5b3d4c5d6e52ef8a0b4a64de341cee0077c5cc6
taaaf11 <taafuuu@gmail.com>
2024-12-29 13:37:46 +0500

    Commit `main.py` to version control

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+from catppuccin import PALETTE
 import flet as ft
+
+
+from model import Link, MusicData
+from ui.link_trees_controls.projects_links_tree_control import ProjectLinksTreeControl
+from ui.music_section_control import MusicSectionControl
+from ui.sections_holder_control import SectionsHolderControl
+
+# source for assets/gray0_ctp_on_line.svg: https://github.com/catppuccin/catppuccin/blob/main/assets/footers/gray0_ctp_on_line.svg
 
 
 def name_container():
     name = ft.Text("Muhammad Altaaf", weight=ft.FontWeight.W_600, size=25)
-    detail = ft.Text("A university undergraduate, persuading computer science major.", size=16)
+    detail = ft.Text("A university undergraduate, pursuing computer science major.", size=16)
     picture = ft.CircleAvatar(
         background_image_url='https://avatars.githubusercontent.com/u/109919009?v=4',
         radius=35,
@@ -28,20 +37,49 @@ def name_container():
 
 
 def main(page: ft.Page):
+    page.scroll = ft.ScrollMode.AUTO
+    use_palette = PALETTE.frappe
+
     page.title = "About me"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
+    page.bgcolor = use_palette.colors.crust.hex
+
     page.fonts = {
-        'Inter': "assets/fonts/Inter-VariableFont_opsz,wght.ttf",
+        'Inter': "fonts/Inter-VariableFont_opsz,wght.ttf",
+        'Comfortaa': "fonts/Comfortaa[wght].ttf",
     }
     page.theme = ft.Theme(font_family='Inter')
 
     page.add(
         ft.Container(
-            name_container(),
-            width=page.window.width - 200,
+            ft.Column(
+                controls=[
+                    name_container(),
+                    SectionsHolderControl(
+                        [
+                            # EducationSectionControl(),
+                            ProjectLinksTreeControl(Link.from_data_file('projects_data.toml')),
+                            MusicSectionControl(musics_data=MusicData.from_data_file())
+                        ]
+                    ),
+                ],
+                spacing=50,
+            ),
+            width=page.width - 200,
             margin=ft.margin.symmetric(vertical=80),
+            expand=True,
+        ),
+        ft.Container(
+            ft.Image(
+                src='gray0_ctp_on_line.svg',
+            ),
+            expand=True
         ),
     )
 
-ft.app(main, assets_dir='assets')
+ft.app(
+    main,
+    assets_dir='assets',
+    view=ft.AppView.WEB_BROWSER
+)
